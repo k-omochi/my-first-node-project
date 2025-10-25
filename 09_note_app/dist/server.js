fd06345cfa233dfd39b70ee4f5af0a2243dd3e89
ubuntu <mizunoawa.web@gmail.com>
2025-10-25 18:04:51 +0900

    記事取得API

--- 09_note_app/dist/server.js
+++ 09_note_app/dist/server.js
@@ -4,7 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const promise_1 = __importDefault(require("mysql2/promise"));
-async function main() {
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+// api
+app.get('/api/journals', async (req, res) => {
     try {
         const connection = await promise_1.default.createConnection({
             host: process.env.DB_HOST,
@@ -15,11 +19,17 @@ async function main() {
         console.log('mysql connected.');
         const [rows] = await connection.query('SELECT * FROM journal');
         console.log(rows);
+        res.json(rows);
         await connection.end();
     }
     catch (err) {
         console.error('connection error: ', err);
     }
-}
-main();
+});
+// -------------------
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log('server started.');
+});
+console.log('server started.');
 //# sourceMappingURL=server.js.map
--- 09_note_app/src/server.ts
+++ 09_note_app/src/server.ts
@@ -1,11 +1,16 @@
 import mysql, { RowDataPacket } from 'mysql2/promise';
+import express, { Request, Response } from 'express';
 
-interface TestRow extends RowDataPacket {
+interface JournalRow extends RowDataPacket {
   id: number;
-  name: string;
+  content: string;
 }
 
-async function main(){
+const app = express();
+app.use(express.json());
+
+// api
+app.get('/api/journals', async (req: Request, res: Response) => {
   try {
     const connection = await mysql.createConnection({
       host: process.env.DB_HOST,
@@ -16,13 +21,20 @@ async function main(){
 
     console.log('mysql connected.');
 
-    const [rows] = await connection.query<TestRow[]>('SELECT * FROM journal');
+    const [rows] = await connection.query<JournalRow[]>('SELECT * FROM journal');
     console.log(rows);
+    
+    res.json(rows);
 
     await connection.end();
   } catch (err) {
     console.error('connection error: ', err);
   }
-}
+});
 
-main();
+// -------------------
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log('server started.');
+});
+console.log('server started.');
